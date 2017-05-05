cf7e5291cdcaa60e55f6ce0bde46ff8aa42f27fa
TigerChung <365@opto.com.tw>
2017-05-05 20:10:50 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,6 @@
+//var visitorAge = prompt("Please enter your age:");
+//document.write(visitorAge);
+var hotText="MDN";
+var URL = 'https://developer.mozilla.org/';
+document.write("Click to return to "+ hotText.link(URL));
+console.log("Program Complete.");
